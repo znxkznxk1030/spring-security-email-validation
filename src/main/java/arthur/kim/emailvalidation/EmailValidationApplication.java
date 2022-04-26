6a99fc8ca973efedfe4c23f9eea33f6ae9a8b1c6
ysKim <znxkznxk1030@gmail.com>
2022-04-26 10:00:48 +0530

    [INIT] First Commit

---
+++ src/main/java/arthur/kim/emailvalidation/EmailValidationApplication.java
@@ -0,0 +1,13 @@
+package arthur.kim.emailvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arthur/kim/emailvalidation/EmailValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package arthur.kim.emailvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
